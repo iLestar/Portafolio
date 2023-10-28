f78626324f64adeb4797e48c3872b6265545788e
iLestar <juanlestar0408@gmail.com>
2023-10-28 16:41:50 -0500

    Prettier & Eslint configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,11 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended'],
+  rules: {
+    'no-console': 'error', // Muestra un error por el uso de `console.log`
+    'vue/multi-word-component-names': 'off' 
+  }
 }
